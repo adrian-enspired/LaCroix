c6a96414ee4dd3413dd7dbda3ffae52c8e49eb23
Robert Winterstein <shizukesa4@gmail.com>
2015-09-07 10:56:40 -0600

    barebones - humble beginnings

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 var template = require('./template/commands.json');
-var commands = require('./lib/commands.js');
-var permit;
+var Commands = require('./lib/commands.js');
 var config   = require('./config.json');
-var redis    = require('redis').createClient();
 var irc      = require('irc');
 var fs       = require('fs');
 
-var client = new irc.Client(config.server, config.nick, {
-    channels: [ config.channel ],
-    floodProtection: true,
-});
+var commands = new Commands(__dirname + '/db.sqlite');
 
-function parse(user, message, callback) {
+/* Parse then execute if the message is a command.
+ * @user: username sending the command
+ * @message: message being sent
+ * @cb(reply, [pm]): callback
+ *      reply: reply message
+ *      pm: true forces any reply as a pm to the sender
+ */
+function parse(user, message, cb) {
     var type = message[0];
     if (type !== "!" && type !== "?") return;
 
@@ -19,48 +21,61 @@ function parse(user, message, callback) {
     var verb    = command[0];
     var params  = command.slice(1);
 
-    redis.hget('command_' + verb, function (err, cmd) {
-        cmd = (typeof template[verb] !== 'undefined') ? template[verb] : cmd;
+    // get command type, built-in or taught?
 
-        if (typeof cmd === 'undefined') {
-            return callback("Command not found.", true);
+    if (!commands.__proto__.hasOwnProperty(verb)) {
+        return cb("Command not found.", true);
+    }
+    var cmd = template[verb];
+
+    if (!isPermitted(user, cmd.permit)) {
+        return cb("You are not permitted to perform this command.", true);
+    }
+
+    if (type === "?") {
+        return cb(cmd.help + "\n\t" + cmd.syntax, true);
+    }
+
+    // log the requesed command
+    console.log('(' + user + '): ' + message);
+
+    if (type === "!") {
+        if (params.length < cmd.params) {
+            return cb("Incorrect number of parameters required:\n" + cmd.syntax, true);
         }
 
-        if (!isPermitted(user, cmd.permit)) {
-            return callback("You are not permitted to perform this command.", true);
-        }
+        commands[cmd.call](params, function (reply) {
+            return cb(reply);
+        });
+    }
+}
 
-        console.log('(' + user + '): ' + message);
-
-        if (type === "?") {
-            return callback(cmd.help + "\n\t" + cmd.desc, true);
-        }
-
-        if (type === "!") {
-            if (params.length < cmd.params) {
-                return callback("Incorrect number of parameters required:\n" + cmd.help, true);
-            }
-            // no commands working!
-            return callback(commands[cmd.call](params));
-        }
+/* Check if user is permitted based on the provided permission level
+ * @name: username to check
+ * @level: the permission level to check against
+ */
+function isPermitted(name, level) {
+    // always allow config.master
+    if (name === config.master) return true;
+    db.each("SELECT * FROM user WHERE name = ?", name, function (err, row) {
+        // deny if user not found
+        if (err || typeof row === 'undefined') return false;
+        // deny banned user
+        if (ROLE[row[1]] === ROLE.ban) return false;
+        // allow user if role is anyone
+        if (level === "*") return true;
+        // allow only if user has permissions equal to or higher
+        return (ROLE[row[1]] >= ROLE[level]) ? true : false;
     });
 }
 
-function isPermitted(user, role) {
-    if (Array.isArray(role)) {
-        for (var r in role) { if (isPermitted(user, r)) return true; }
-        return false;
-    }
-    // always allow config.master
-    if (user === config.master) return true;
-    // deny user if blacklisted
-    if (permit.blacklist.indexOf(user) > -1) return false;
-    // allow user if role contains "*" (anyone) wildcard
-    if (role === "*" || permit[role].indexOf("*") > -1) return true;
-    // allow user if they are listed for that role
-    return (permit[role].indexOf(user) > -1) ? true : false;
-}
+// create irc client
+var client = new irc.Client(config.server, config.nick, {
+    channels: [ config.channel ],
+    floodProtection: true,
+});
 
+// listen for all messages in irc
 client.addListener('message', function (from, to, message) {
     if (from === config.nick) return;
     parse(from, message, function (reply, pm) {
@@ -70,20 +85,7 @@ client.addListener('message', function (from, to, message) {
     });
 });
 
+// irc client errors
 client.on('error', function (err) {
-    console.error(err);
-});
-
-redis.hgetall("permissions", function (err, value) {
-    // test / change
-    permit = value || {
-        masters: [],
-        teachers: [],
-        users: ["*"],
-        blacklist: [],
-    };
-});
-
-redis.on('error', function (err) {
     console.error(err);
 });
