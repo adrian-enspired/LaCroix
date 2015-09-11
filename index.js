30f06325d8d46bbe9f78af1fdd0768204616da67
Robert Winterstein <shizukesa4@gmail.com>
2015-09-11 11:55:04 -0600

    better command detection and improved command workflow

--- index.js
+++ index.js
@@ -15,11 +15,68 @@ var client = new irc.Client(config.server, config.nick, {
 
 // listen for all messages in irc
 client.addListener('message', function (from, to, message) {
-    if (from === config.nick) return;
-    commands.parse(from, message, function (reply, pm) {
-        var target = (to === config.nick || pm) ? from : config.channel;
-        console.log('(' + config.nick + '): ' + reply);
-        client.say(target, reply);
+
+    var cmd = commands.parseCommand(from, to, message, function (err, cmd) {
+        if (err) {
+            console.error(err);
+            client.say(from, err);
+            return;
+        }
+
+        // valid command, log attempt
+        console.log('(' + from + '): ' + message);
+
+        commands.isPermitted(cmd, function (err, permit) {
+            if (err) {
+                console.error(err);
+                client.say(cmd.sender, err);
+                return;
+            }
+
+            if (!permit) {
+                client.say(cmd.sender, "You are not permitted to perform this command.");
+                return;
+            }
+
+            // if help
+            if (cmd.prefix === "?") client.say(cmd.sender, cmd.template.help + "\n\t" + cmd.template.syntax);
+
+            // if command
+            if (cmd.prefix === "!") {
+                // parameter check?
+
+                // **********************************************
+                // Dude, clean this up! D:
+                if (typeof cmd.template.call !== 'undefined') {
+                    commands.calls[cmd.template.call].call(commands, cmd, function (err, args) {
+                        if (err) {
+                            console.error(err);
+                            client.say(cmd.sender, err);
+                            return;
+                        }
+                        console.log(args);
+                        cmd.args = args;
+                        commands.parseReply(cmd, function (err, response) {
+                            if (err) {
+                                console.error(err);
+                                client.say(cmd.sender, err);
+                                return;
+                            }
+                            client.say(cmd.target, response);
+                        });
+                    });
+                } else {
+                    commands.parseReply(cmd, function (err, response) {
+                        if (err) {
+                            console.error(err);
+                            client.say(cmd.sender, err);
+                            return;
+                        }
+                        client.say(cmd.target, response);
+                    });
+                }
+            }
+        });
     });
 });
 
