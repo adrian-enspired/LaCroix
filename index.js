64d2b54c4fbb1dec648d0af5ba98473c13a56c61
Robert Winterstein <shizukesa4@gmail.com>
2015-09-11 12:08:03 -0600

    better than awful error handling

--- index.js
+++ index.js
@@ -13,30 +13,25 @@ var client = new irc.Client(config.server, config.nick, {
     floodProtection: true,
 });
 
+function error(err, target) {
+    console.error(err);
+    if (typeof target !== 'undefined') client.say(target, err);
+}
+
 // listen for all messages in irc
 client.addListener('message', function (from, to, message) {
 
     var cmd = commands.parseCommand(from, to, message, function (err, cmd) {
-        if (err) {
-            console.error(err);
-            client.say(from, err);
-            return;
-        }
+        if (err) return error(err, cmd.sender);
 
         // valid command, log attempt
         console.log('(' + from + '): ' + message);
 
         commands.isPermitted(cmd, function (err, permit) {
-            if (err) {
-                console.error(err);
-                client.say(cmd.sender, err);
-                return;
-            }
+            if (err) return error(err, cmd.sender);
 
-            if (!permit) {
-                client.say(cmd.sender, "You are not permitted to perform this command.");
-                return;
-            }
+            if (!permit)
+                return error("You are not permitted to perform this command.", cmd.sender);
 
             // if help
             if (cmd.prefix === "?") client.say(cmd.sender, cmd.template.help + "\n\t" + cmd.template.syntax);
@@ -49,29 +44,17 @@ client.addListener('message', function (from, to, message) {
                 // Dude, clean this up! D:
                 if (typeof cmd.template.call !== 'undefined') {
                     commands.calls[cmd.template.call].call(commands, cmd, function (err, args) {
-                        if (err) {
-                            console.error(err);
-                            client.say(cmd.sender, err);
-                            return;
-                        }
-                        console.log(args);
+                        if (err) return error(err, cmd.sender);
+
                         cmd.args = args;
                         commands.parseReply(cmd, function (err, response) {
-                            if (err) {
-                                console.error(err);
-                                client.say(cmd.sender, err);
-                                return;
-                            }
+                            if (err) return error(err, cmd.sender);
                             client.say(cmd.target, response);
                         });
                     });
                 } else {
                     commands.parseReply(cmd, function (err, response) {
-                        if (err) {
-                            console.error(err);
-                            client.say(cmd.sender, err);
-                            return;
-                        }
+                        if (err) return error(err, cmd.sender);
                         client.say(cmd.target, response);
                     });
                 }
@@ -82,5 +65,5 @@ client.addListener('message', function (from, to, message) {
 
 // irc client errors
 client.on('error', function (err) {
-    console.error(err);
+    error(err, config.master);
 });
